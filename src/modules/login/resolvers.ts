5ed2dcbafe4b706bb2c32ee3462e0848b6b833df
Paul Bushuo Buschmann <paul.buschmann@chello.at>
2020-01-23 08:54:17 +0100

    feat: OAuthTwitter

--- src/modules/login/resolvers.ts
+++ src/modules/login/resolvers.ts
@@ -48,7 +48,10 @@ export const resolvers: ResolverMap = {
                 ];
             }
 
-            const valid = await bcrypt.compare(password, user.password);
+            const valid = await bcrypt.compare(
+                password,
+                user.password as string
+            );
             if (!valid) {
                 return errorResponse;
             }
--- src/startServer.ts
+++ src/startServer.ts
@@ -5,18 +5,21 @@ import * as session from 'express-session';
 import * as connectRedis from 'connect-redis';
 import * as RateLimit from 'express-rate-limit';
 import * as RateLimitRedisStore from 'rate-limit-redis';
+import * as passport from 'passport';
+import { Strategy } from 'passport-twitter';
 import { GraphQLServer } from 'graphql-yoga';
 import { createTypeormConnection } from './utils/createTypeormConnection';
 import { redis } from './redis';
 import { confirmEmail } from './routes/confirmEmail';
 import { genSchema } from './utils/generateSchema';
 import { REDIS_SESSION_PREFIX } from './constants';
+import { User } from './entity/User';
 
 // tslint:disable-next-line: variable-name
 const RedisStore = connectRedis(session);
 
 export const startServer = async () => {
-    await createTypeormConnection();
+    const con = await createTypeormConnection();
 
     const server = new GraphQLServer({
         schema: genSchema(),
@@ -63,6 +66,62 @@ export const startServer = async () => {
     };
 
     server.express.get('/confirm/:id', confirmEmail);
+
+    passport.use(
+        new Strategy(
+            {
+                consumerKey: process.env.TWITTER_CONSUMER_KEY as string,
+                consumerSecret: process.env.TWITTER_CONSUMER_SECRET as string,
+                callbackURL: 'http://localhost:4000/auth/twitter/callback',
+                includeEmail: true
+            },
+            async (_, __, profile, done) => {
+                console.log(profile);
+                const { id, emails } = profile;
+
+                const query = con
+                    .getRepository(User)
+                    .createQueryBuilder('user')
+                    .where('user.twitterId = :id', { id });
+                let email: string | null = null;
+
+                if (emails) {
+                    email = emails[0].value;
+                    query.orWhere('user.email = :email', { email });
+                }
+                let user = await query.getOne();
+
+                if (!user) {
+                    user = await User.create({
+                        twitterId: id,
+                        email
+                    }).save();
+                } else if (!user.twitterId) {
+                    // we found user by email => already registered but not with twitter OAuth
+                    user.twitterId = id;
+                    await user.save();
+                } else {
+                    // we have twitterId
+                    // login
+                }
+
+                return done(null, { id: user.id });
+            }
+        )
+    );
+
+    server.express.use(passport.initialize());
+
+    server.express.get('/auth/twitter', passport.authenticate('twitter'));
+    server.express.get(
+        '/auth/twitter/callback',
+        passport.authenticate('twitter', { session: false }),
+        (req, res) => {
+            (req.session as any).userId = (req.user as any).id;
+            // @todo successful authentication, redirect to frontend
+            res.redirect('/');
+        }
+    );
 
     const app = await server.start({
         cors,
